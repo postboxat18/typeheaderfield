bfced3400cd8531853b1af7ab09aac6c5f11a81e
AravinthC-18 <aravinth@aostasoftware.com>
2024-03-07 14:56:10 +0530

    commit 0.0.4

--- lib/typeheaderfield.dart
+++ lib/typeheaderfield.dart
@@ -4,3 +4,4 @@ export 'package:typeheaderfield/src/common/base/suggestions_controller.dart';
 export 'package:typeheaderfield/src/common/field/typeahead_field.dart';
 export 'package:typeheaderfield/src/material/typeheaderfield.dart';
 export 'package:typeheaderfield/src/cupertino/typeahead_field.dart';
+export 'package:typeheaderfield/src/material/field/text_field_configuration.dart';
